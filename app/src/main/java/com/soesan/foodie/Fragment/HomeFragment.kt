c61837929ffdefc8d6af1e10be55bf6c1c243e72
soesandanwe <soesandanwe@gmail.com>
2019-07-03 11:33:02 +0800

    Add Action in Button

--- app/src/main/java/com/soesan/foodie/Fragment/HomeFragment.kt
+++ app/src/main/java/com/soesan/foodie/Fragment/HomeFragment.kt
@@ -3,11 +3,17 @@ package com.soesan.foodie.Fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 import com.soesan.foodie.R
+import com.soesan.foodie.UI.TopPanelButtonView
+import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,8 +31,13 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View=inflater.inflate(R.layout.fragment_home, container, false)
+        val btn_popular: TopPanelButtonView = view.findViewById(R.id.btn_popular)
+        btn_popular.setOnClickListener{ view ->  Toast.makeText(context,"Hello",Toast.LENGTH_LONG).show()}
+        return  view
     }
+
+
 
 
 }
