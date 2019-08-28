096b8aa5ead1bdf00a9c69c3b9cf5a1e37c33ec6
soesandanwe <soesandanwe@gmail.com>
2019-08-28 19:45:13 +0800

    Fix for map smooth and add .. for see more text

--- app/src/main/java/com/soesan/foodie/Activity/MostPopularShops.kt
+++ app/src/main/java/com/soesan/foodie/Activity/MostPopularShops.kt
@@ -1,9 +1,16 @@
 package com.soesan.foodie.Activity
 
+import android.Manifest
 import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Paint
+import android.location.Location
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -19,22 +26,37 @@ import com.google.android.gms.maps.model.MarkerOptions
 import android.view.ViewGroup
 import com.google.android.gms.maps.*
 import android.util.DisplayMetrics
+import android.view.ViewStub
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.CameraUpdate
 import kotlin.math.roundToInt
+import com.google.android.gms.maps.SupportMapFragment
+
+import android.support.v4.app.FragmentManager
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.CardView
+import android.util.AttributeSet
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
 
-class MostPopularShops : AppCompatActivity(), AdapterView.OnItemSelectedListener, OnMapReadyCallback {
+
+
+
+class MostPopularShops : AppCompatActivity(), AdapterView.OnItemSelectedListener, OnMapReadyCallback,
+    GoogleMap.OnMyLocationClickListener, GoogleMap.OnMyLocationButtonClickListener {
     var spinner: Spinner? = null
     var shops: Int=20
+    var MY_LOCATION_REQUEST_CODE:Int=1
     private lateinit var mMap: GoogleMap
-    private lateinit var mapView:MapView
-    private val MAP_VIEW_BUNDLE_KEY = "AIzaSyAByJNQVORLPeY_3g3Y5KssRM3-PHkbsjw"
+
+
+    //keytool -list -v -keystore "C:\Users\soe.sanda\.android\debug.keystore" -alias androiddebugkey -storepass android -keypass android
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_most_popular_shops)
-      //  setSupportActionBar(findViewById(R.id.main_toolbar));
+
 
         val recyclerViewPopular = findViewById<RecyclerView>(R.id.recyclerView_mostpopular)
         val mostPopularAdapter = MostPopularAdapter(shops,this )
@@ -43,7 +65,7 @@ class MostPopularShops : AppCompatActivity(), AdapterView.OnItemSelectedListener
         recyclerViewPopular.adapter = mostPopularAdapter
         recyclerViewPopular.setHasFixedSize(true)
 
-
+        //Sliding Up Pannel
         val mLayout = findViewById<SlidingUpPanelLayout>(R.id.sliding_layout)
         mLayout.setDragView(R.id.dragView)
         mLayout.addPanelSlideListener(object : SlidingUpPanelLayout.PanelSlideListener {
@@ -83,28 +105,22 @@ class MostPopularShops : AppCompatActivity(), AdapterView.OnItemSelectedListener
         // Set Adapter to Spinner
         spinner!!.setAdapter(aa)
 
-        var mapViewBundle: Bundle? = null
-        if (savedInstanceState != null) {
-
-            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY)
-        }
-
-
-        mapView = findViewById(R.id.mapView_shop)
-        mapView.onCreate(mapViewBundle)
-        mapView.getMapAsync(this)
-        val mapViewParams = mapView.getLayoutParams()
-
         val displayMetrics = DisplayMetrics()
         this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics)
         val screenDisplayHeight = displayMetrics.heightPixels
-        val bottom_slide_dimension = this.resources.getDimension(R.dimen.bottom_slide).toInt();
-        val margin_dimension = this.resources.getDimension(R.dimen.activity_horizontal_margin).toInt();
+        val bottom_slide_dimension =mLayout.panelHeight
+        val mapLayout=findViewById<View>(R.id.mapViewLayout)
+        mapLayout.layoutParams.height= (screenDisplayHeight-(bottom_slide_dimension*2))
 
-        mapViewParams.height = (screenDisplayHeight-bottom_slide_dimension)-((bottom_slide_dimension/2)+margin_dimension);
-        mapView.setLayoutParams(mapViewParams);
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapViewLayout) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
 
     }
+
+
     override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
 
     }
@@ -112,16 +128,16 @@ class MostPopularShops : AppCompatActivity(), AdapterView.OnItemSelectedListener
     override fun onNothingSelected(arg0: AdapterView<*>) {
 
     }
-
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        mMap.setMinZoomPreference(5f);
+        mMap.setMinZoomPreference(5f)
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15f))
+
 
         val mUiSettings = mMap.getUiSettings()
         mUiSettings.setZoomControlsEnabled(true)
@@ -131,5 +147,54 @@ class MostPopularShops : AppCompatActivity(), AdapterView.OnItemSelectedListener
         mUiSettings.setCompassEnabled(true)
         mUiSettings.setMyLocationButtonEnabled(true)
         mUiSettings.setAllGesturesEnabled(true)
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+
+            mMap.setMyLocationEnabled(true);
+            mMap.setOnMyLocationButtonClickListener(this);
+            mMap.setOnMyLocationClickListener(this);
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                MY_LOCATION_REQUEST_CODE)
+
+        }
+
     }
+
+    override fun onMyLocationButtonClick(): Boolean {
+
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+
+        return false;
+    }
+
+    override fun onMyLocationClick(location: Location) {
+
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_LOCATION_REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    mMap.setMyLocationEnabled(true);
+                } else {
+                    mMap.setMyLocationEnabled(false);
+                }
+                return
+            }
+
+            else -> {
+                mMap.setMyLocationEnabled(false);
+            }
+        }
+    }
+
 }
